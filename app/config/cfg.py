67e64d9480c01957d9fa52711e66b777241ddfed
dan.zhou <dan.zhou@derbysoft.com>
2017-11-09 12:46:15 +0800

    小修改 新增importUtil

--- app/config/cfg.py
+++ app/config/cfg.py
@@ -2,9 +2,15 @@ import configparser
 
 
 class fligtConfig:
-    def __init__(self):
-        cfg = configparser.ConfigParser()
-        cfg.read("cfg.ini")
 
+    def __init__(self):
+        self.cfg = configparser.ConfigParser()
+        self.cfg.read("cfg.ini")
+
+    def  get(self, section, option):
+        return self.cfg.get(section, option)
+
+    def  getint(self, section, option):
+        return self.cfg.getint(section, option)
 
 cfg = fligtConfig()
